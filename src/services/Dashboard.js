b4a8e36fa02962c3a43824ce3378c6829edf4010
Isah Hamza <itshamzy@gmail.com>
2024-09-05 19:45:52 +0100

    -

---
+++ src/services/Dashboard.js
@@ -0,0 +1,67 @@
+"use client";
+
+import endpoints from "../api/endpoints";
+import { axiosClient } from "../api/axiosClient";
+
+const GetDashboardStats = (admin_id) => {
+  return axiosClient()
+  .get(`${endpoints.dashbaord.DASHBOARD_STATS}?admin_id=${admin_id}`)
+  .then((res) => res)
+  .catch((error) => Promise.reject(error));
+}
+
+const GetReferralStats = (admin_id) => {
+  return axiosClient()
+  .get(`${endpoints.dashbaord.REFERRAL_STATS}?admin_id=${admin_id}`)
+  .then((res) => res)
+  .catch((error) => Promise.reject(error));
+}
+
+const GetActivities = (doctor_id) => {
+  return axiosClient()
+    .get(`${endpoints.dashbaord.ACTIVITIES}?doctor_id=${doctor_id}`)
+    .then((res) => res)
+    .catch((error) => Promise.reject(error));
+}
+
+const GetRebateEarnings = (doctor_id) => {
+  return axiosClient()
+    .get(`${endpoints.dashbaord.REBATE_EARNINGS}?doctor_id=${doctor_id}`)
+    .then((res) => res)
+    .catch((error) => Promise.reject(error));
+}
+
+const GetDashboardDetails = (doctor_id) => {
+  return axiosClient()
+    .get(`${endpoints.dashbaord.DASHBOARD_DETAILS}?doctor_id=${doctor_id}`)
+    .then((res) => res)
+    .catch((error) => Promise.reject(error));
+}
+
+const GetRebateChart = (admin_id) => {
+  return axiosClient()
+    .get(`${endpoints.dashbaord.REBATE_CHART}?admin_id=${admin_id}`)
+    .then((res) => res)
+    .catch((error) => Promise.reject(error));
+}
+
+const GetAppointmentStats = (admin_id) => {
+  return axiosClient()
+    .get(`${endpoints.dashbaord.APPOINTMENT_STATS}?admin_id=${admin_id}`)
+    .then((res) => res)
+    .catch((error) => Promise.reject(error));
+}
+
+const GetNotifications = () => {
+  return axiosClient()
+    .get(`${endpoints.dashbaord.ACTIVITIES}`)
+    .then((res) => res)
+    .catch((error) => Promise.reject(error));
+}
+
+
+export default {
+  GetDashboardDetails, GetRebateEarnings, GetActivities, GetDashboardStats, GetReferralStats,
+  GetRebateChart, GetAppointmentStats, GetNotifications
+};
+
