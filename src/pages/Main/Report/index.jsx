e10472a0291e2c1d8467a56d83e8000ec0f0a225
Isah Hamza <itshamzy@gmail.com>
2024-10-08 21:58:09 +0100

    -

--- src/pages/Main/Report/index.jsx
+++ src/pages/Main/Report/index.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 import Select from '../../../components/Inputs/Select'
 import { MdArrowForward } from 'react-icons/md';
-import { PieChart } from 'recharts';
+import PiechartWithNeedle from '../../../components/Chart/PieChartWithNeedle';
+import MultipleBarChart from '../../../components/Chart/MultipleBarCharts';
+import PieChart from '../../../components/Chart/PieChart';
 
 const Report = () => {
 
@@ -25,7 +27,7 @@ const Report = () => {
         </div>
         <Select className={'min-w-[120px]'} options={durations} />
       </div>
-      <div className="mt-7 flex">
+      <div className="mt-7 flex gap-5">
         <div className="w-2/5 grid grid-cols-2 gap-5">
           <div className="bg-white rounded-lg px-4 p-5 border col-span-2">
               <p>Total Referrals</p>
@@ -66,20 +68,94 @@ const Report = () => {
             </div>
           </div>
         </div>
+        <div className="flex-1">
+          <div className="flex gap-5 w-full">
+              <div className="w-full bg-white h-full p-5 rounded-lg border">
+                  <div className="flex items-center justify-between pb-3 border-b">
+                        <p className='font-semibold' >Conversion Rate</p>
+                  </div>
+                  <div className="mt-3"> 
+                        <p className='text-sm' >Metric showing the referrals that are successfully converted into appointments.  </p> 
+                      <div className="grid grid-cols-2 gap-3 mt-10">
+                          <div className="flex flex-col gap-10">
+                              <div className="flex items-center gap-2">
+                                  <p className='font-semibold'>{ 303}</p>
+                                  <div className="text-sm flex items-center gap-1">
+                                      <span>Converted Referrals</span>
+                                  </div>
+                              </div>
+                              <div className="flex items-center gap-2">
+                                  <p className='font-semibold'>{ 21}</p>
+                                  <div className="text-sm flex items-center gap-1">
+                                      <span>Converted Referrals</span>
+                                  </div>
+                              </div>
+                              <div className="flex items-center gap-2">
+                                  <p className='font-semibold'>{ 345}</p>
+                                  <div className="text-sm flex items-center gap-1">
+                                      <span>Total Referrals</span>
+                                  </div>
+                              </div>
+                              
+                          </div> 
+                          <div className="-ml-10 h-[300px] -mt-20">
+                              <PiechartWithNeedle />
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div> 
+        </div>
+      </div>
+      <div className="mt-7 flex gap-5">
         <div className="w-3/5">
-        <div className="flex gap-5 mt-5">
-                { department !== 'Rebate Unit' ? <div className="bg-white min-w-[35%] h-full p-5 rounded-lg border">
-                    <div className="flex items-center justify-between pb-3 border-b">
-                         <p className='font-semibold' >Referrals Stats</p>
+        <div className="w-full bg-white h-full p-5 rounded-lg border">
+                <div className="flex items-center justify-between pb-3 border-b">
+                      <p className='font-semibold' >Comparative Analysis</p>
+                </div>
+                <div className="mt-3"> 
+                      <p className='text-sm' >Comparison of patient referrals, booked appointments, and payments made by referrers. </p> 
+                    <div className="">
+                      <div className="mb-2 flex justify-center items-center text-center gap-10">
+                          <div className="">
+                              <div className="text-sm flex items-center gap-1">
+                                  <div className="w-2 h-2 rounded-full bg-[#00C49F]"></div>
+                                  <span>Referral</span>
+                              </div>
+                          </div>
+                          <div className="">
+                              <div className="text-sm flex items-center gap-1">
+                                  <div className="w-2 h-2 rounded-full bg-light_blue"></div>
+                                  <span>Appointment</span>
+                              </div>
+                          </div>
+                          <div className="">
+                              <div className="text-sm flex items-center gap-1">
+                                  <div className="w-2 h-2 rounded-full bg-slate-400"></div>
+                                  <span>Payment Mode</span>
+                              </div>
+                          </div>
+                      </div> 
+                      <div className="-ml-7 w-[110%] h-[300px]">
+                        <MultipleBarChart />
+                      </div>
                     </div>
-                    <div className="mt-3"> 
-                          <p className='text-sm' >Analysis of daily pending & completed tests </p> 
-                        <div className="flex flex-col">
-                            <div className=" -ml-10 h-[250px]">
-                                <PieChart 
-                                    completed = {44} 
-                                    pending= {24} 
-                                />
+                </div>
+            </div> 
+        </div>
+        <div className="w-2/5">
+          <div className="w-full bg-white h-full p-5 rounded-lg border">
+              <div className="flex items-center justify-between pb-3 border-b">
+                    <p className='font-semibold' >Referral Stats</p>
+              </div>
+              <div className="mt-3"> 
+                    <p className='text-sm' >Analysis of pending & completed referrals. </p> 
+                    <div className="flex flex-col mt-10">
+                            <div className="justify-center items-center text-center -ml-10 h-[250px]">
+                                <PieChart
+                                    completed = {40} 
+                                    pending= { 50} 
+                                    />
                             </div>
                             <div className="flex justify-center items-center text-center gap-10">
                                 <div className="">
@@ -87,20 +163,19 @@ const Report = () => {
                                         <div className="w-2 h-2 rounded-full bg-[#00C49F]"></div>
                                         <span>Completed</span>
                                     </div>
-                                    <p className='pl-'>{ (department == 'Test Unit' || department == 'Laboratory Services' || department == 'Radiology' || department == 'Result Unit') ? testStats?.percentage?.completed_tests : referralStats?.completed}</p>
+                                    <p className='pl-'>{ 40}</p>
                                 </div>
                                 <div className="">
                                     <div className="text-sm flex items-center gap-1">
                                         <div className="w-2 h-2 rounded-full bg-light_blue"></div>
                                         <span>Pending</span>
                                     </div>
-                                    <p className='pl-'>{(department == 'Test Unit' || department == 'Laboratory Services' || department == 'Radiology' || department == 'Result Unit') ? testStats?.percentage?.pending_tests : referralStats?.pending}</p>
+                                    <p className='pl-'>{44}</p>
                                 </div>
                             </div> 
                         </div>
-                    </div>
-                </div> : null}
-            </div> 
+              </div>
+          </div>
         </div>
       </div>
     </div>
